e5d08709ef5c548c6cc762c3b5814fbf096dd509
Jakub Jareš <me@jakubjares.com>
2025-06-25 17:23:33 +0200

    Fix randomness

--- Filegenerator.ps1
+++ Filegenerator.ps1
@@ -18,15 +18,15 @@ Measure-Command {
         $rg = $_
         foreach ($i in $rg) {
             $ts = [datetime]::now.AddSeconds(-$i).ToString('yyyy-MM-dd HH:mm:ss')
-            $plc = $pn[$r.Next(0, $pn.Length-1)]
+            $plc = $pn[$r.Next(0, $pn.Length)]
             $o = $r.Next(101,121)
             $b = $r.Next(1000,1101)
-            $s = $sc[$r.Next(0, $sc.Length-1)]
+            $s = $sc[$r.Next(0, $sc.Length)]
             $mt = [math]::Round($r.Next(60, 110) + $r.Next(),2)
             $l = $r.Next(0,101)
         
             if ($r.Next(1,8) -eq 4) {
-                $ei = $r.Next(0, $ets.Length-1)
+                $ei = $r.Next(0, $ets.Length)
                 if (0 -eq $ei) {
                     $v = $r.Next(1,11)
                     $ms = "ERROR; {0}; {1}; Sandextrator overload; {2}; {3}; {4}; {5}; {6}; {7}" -f $ts, $plc, $v, $s, $o, $b, $mt, $l
