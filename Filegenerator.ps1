5106ba89907796eb96e4aa9a045184e9a30b6fca
Jakub Jareš <me@jakubjares.com>
2025-06-25 15:57:21 +0200

    solution

--- Filegenerator.ps1
+++ Filegenerator.ps1
@@ -1,40 +1,48 @@
-Measure-Command{
-$bigFileName = "plc_log.txt"
-$plcNames = 'PLC_A','PLC_B','PLC_C','PLC_D'
-$errorTypes = @(
-    'Sandextrator overload',
-    'Conveyor misalignment',
-    'Valve stuck',
-    'Temperature warning'
-)
-$statusCodes = 'OK','WARN','ERR'
- 
-$logLines = @()
- 
-for ($i=0; $i -lt 50000; $i++) {
-    $timestamp = (Get-Date).AddSeconds(-$i).ToString("yyyy-MM-dd HH:mm:ss")
-    $plc = $plcNames | Get-Random
-    $operator = Get-Random -Minimum 101 -Maximum 121
-    $batch = Get-Random -Minimum 1000 -Maximum 1101
-    $status = $statusCodes | Get-Random
-    $machineTemp = [math]::Round((Get-Random -Minimum 60 -Maximum 110) + (Get-Random),2)
-    $load = Get-Random -Minimum 0 -Maximum 101
- 
-    if ((Get-Random -Minimum 1 -Maximum 8) -eq 4) {
-        $errorType = $errorTypes | Get-Random
-        if ($errorType -eq 'Sandextrator overload') {
-            $value = (Get-Random -Minimum 1 -Maximum 11)
-            $msg = "ERROR; $timestamp; $plc; $errorType; $value; $status; $operator; $batch; $machineTemp; $load"
-        } else {
-            $msg = "ERROR; $timestamp; $plc; $errorType; ; $status; $operator; $batch; $machineTemp; $load"
+Measure-Command {
+
+    $bfn = "plc_log.txt"
+    $bs = @(0..24999), @(25000..49999)
+
+
+    $ll = foreach ($_ in  $bs)<# -ThrottleLimit 2 -Parallel #> {
+    $pn = 'PLC_A','PLC_B','PLC_C','PLC_D'
+    $ets = @(
+        'Sandextrator overload',
+        'Conveyor misalignment',
+        'Valve stuck',
+        'Temperature warning'
+    )
+    $sc = 'OK','WARN','ERR'
+    $r = [System.Random]::new()
+
+        $rg = $_
+        foreach ($i in $rg) {
+            $ts = [datetime]::now.AddSeconds(-$i).ToString('yyyy-MM-dd HH:mm:ss')
+            $plc = $pn[$r.Next(0, $pn.Length-1)]
+            $o = $r.Next(101,121)
+            $b = $r.Next(1000,1101)
+            $s = $sc[$r.Next(0, $sc.Length-1)]
+            $mt = [math]::Round($r.Next(60, 110) + $r.Next(),2)
+            $l = $r.Next(0,101)
+        
+            if ($r.Next(1,8) -eq 4) {
+                $ei = $r.Next(0, $ets.Length-1)
+                if (0 -eq $ei) {
+                    $v = $r.Next(1,11)
+                    $ms = "ERROR; {0}; {1}; Sandextrator overload; {2}; {3}; {4}; {5}; {6}; {7}" -f $ts, $plc, $v, $s, $o, $b, $mt, $l
+                } else {
+                    $et =  $ets[$ei]
+                    $ms = 'ERROR; {0}; {1}; {2}; ; {3}; {4}; {5}; {6}; {7}' -f $ts, $plc, $et, $s, $o, $b, $mt, $l
+                }
+            } else {
+                $ms = 'INFO; {0}; {1}; System running normally; ; {2}; {3}; {4}; {5}; {6}' -f $ts, $plc, $s, $o, $b, $mt, $l
+            }
+        
+            $ms
         }
-    } else {
-        $msg = "INFO; $timestamp; $plc; System running normally; ; $status; $operator; $batch; $machineTemp; $load"
     }
- 
-    $logLines += $msg
+    
+   [system.io.file]::WriteAllLines($bfn, $ll)
+   [console]::writeLine("PLC log file generated.") # looks like this is part of the requirements? othwerwise we can save more time
 }
- 
-Set-Content -Path $bigFileName -Value $logLines
-Write-Output "PLC log file generated."
-}
+
